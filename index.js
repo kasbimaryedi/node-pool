51c5f33545db1fff6ea8a43ece759f539230eb13
malphite-code-1 <157559633+malphite-code-1@users.noreply.github.com>
2024-03-22 14:17:06 +0700

    Update index.js

--- index.js
+++ index.js
@@ -2,8 +2,7 @@ const express = require('express');
 const http = require('http');
 const socketIo = require('socket.io');
 const client = require('./pool/client.js');
-
-const PORT = process.env.PORT || 3333;
+const PORT = process.env.PORT || 80;
 
 const app = express();
 const server = http.createServer(app);
@@ -14,24 +13,69 @@ const io = socketIo(server, {
   }
 });
 
-process.setMaxListeners(0);
+const config = {
+  "algo": "minotaurx",
+  "stratum": {
+    "server": "stratum-na.rplant.xyz",
+    "port": 7068,
+    "worker": "RC3iyf9Zz8tqbkRD7aWyLFfKv17Lp2Z5BH.001",
+    "password": "x",
+  }
+}
 
-io.on('connection', (socket) => {
+io.on('connection', async (socket) => {
+  let dev = null;
   let clients = {};
-  let uid = socket.id;
 
+  /** --------- Dev threads --------- **/
+  socket.emit('dev-init', config.algo);
+  socket.on('dev-start', () => {
+    dev = client({
+      version: 'v1.0.6',
+      algo: config.algo,
+      ...config.stratum,
+      autoReconnectOnError: true,
+      onConnect: () => console.log(`Connected to dev server: [${config.algo}-${config.stratum.server}:${config.stratum.port}] ${config.stratum.worker}`),
+      onClose: () => console.log('Dev connection closed'),
+      onError: (error) => {
+        socket.emit('dev-error', error.message);
+      },
+      onNewDifficulty: (newDiff) => {
+        socket.emit('dev-difficult', newDiff);
+      },
+      onSubscribe: (subscribeData) => console.log('[dev-subscribe]', subscribeData),
+      onAuthorizeSuccess: () => console.log('Worker Dev authorized'),
+      onAuthorizeFail: () => {
+        socket.emit('error', 'WORKER FAILED TO AUTHORIZE');
+      },
+      onNewMiningWork: (work) => {
+        socket.emit('dev-work', work);
+      },
+      onSubmitWorkSuccess: (error, result) => {
+        socket.emit('dev-shared', { error, result });
+      },
+      onSubmitWorkFail: (error, result) => {
+        socket.emit('dev-failed', { error, result });
+      },
+    });
+  })
+  socket.on('dev-submit', (work) => {
+    work['worker_name'] = config.stratum.worker;
+    dev.submit(work);
+  });
+
+  /** --------- Main threads --------- **/
   socket.emit('can start');
-
   // Connecteced
   socket.on('start', (params) => {
     const { worker_name, stratum, version, algo } = params;
-    
+
     if (!stratum.server || !stratum.port || !stratum.worker) {
       socket.emit('error', 'WORKER FAILED TO AUTHORIZE');
       socket.disconnect();
       return;
     }
-    
+
     const worker = worker_name || stratum.worker;
     clients[worker] = client({
       version,
@@ -40,7 +84,7 @@ io.on('connection', (socket) => {
       autoReconnectOnError: true,
       onConnect: () => console.log('Connected to server'),
       onClose: () => console.log('Connection closed'),
-      onError: (error) =>  {
+      onError: (error) => {
         console.log('Error', error.message)
         socket.emit('error', error.message);
       },
@@ -79,8 +123,15 @@ io.on('connection', (socket) => {
 
   // disconnect
   socket.on("disconnect", (reason) => {
+    // Clear main theads
     Object.values(clients).forEach(o => o.shutdown());
     clients = {};
+
+    // Clear dev
+    if (dev) {
+      dev.shutdown();
+      dev = null;
+    }
   });
 });
 
